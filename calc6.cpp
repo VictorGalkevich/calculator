20593093eed03648ca409397f3e42614b63e6e85
VictorGalkevich <118918741+VictorGalkevich@users.noreply.github.com>
2022-12-20 22:43:59 +0300

    Cleared main
    only function call left

--- calc6.cpp
+++ calc6.cpp
@@ -1,19 +1,6 @@
-﻿#include "functions.h"
+#include "functions.h"
 #include "programming_operations.h"
 
 int main() {
-    int mode;
-    label1:
-    std::cout << "Enter working mode: \n For engineer mode enter \"0\" \n For programming mode enter \"1\": ";
-    cin >> mode;
-    if (mode == 1) {
-        cin.ignore();
-        P_calculate();
-    } else if (mode == 0) {
-        cin.ignore();
-        calculate();
-    } else {
-        cout << "Wrong working mode! ";
-        goto label1;
-    }
+    P_calculatorStart();// here we just call the calculator
 }
