5d5c0262546512ad630d1b34bcaf11df87c6263a
VictorGalkevich <118918741+VictorGalkevich@users.noreply.github.com>
2022-12-20 22:49:17 +0300

    added commentaries and new logic

--- programming_operations.h
+++ programming_operations.h
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+//struct to analyze the stroke in programming mode
+
 struct int_identifier {
     int value;
     char ch;
@@ -12,7 +14,11 @@ struct int_identifier {
     int_identifier() : value(0), ch(' ') {};
 };
 
+//method to get the priority(rank) in programming mode
+
 int P_get_prior(char ch);
+
+//here we declare the operations
 
 string hex(double x);
 
@@ -20,33 +26,37 @@ string bin(double x);
 
 string oct(double x);
 
-void P_add_number(stack<int_identifier> &numbers_list, int_identifier &item1, bool &flag1);
+//declare the functions that add to the stacks
 
-void P_add_el(stack<int_identifier> &operators_list, int_identifier &item1, char element);
+void P_add_number(stack<int_identifier>& numbers_list, int_identifier& item1, bool& flag1);
 
-void add_not(stack<int_identifier> &operators_list, int_identifier &item1);
+void P_add_el(stack<int_identifier>& operators_list, int_identifier& item1, char element);
 
-void P_sum(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-           stack<int_identifier> &operators_list, int c);
+void add_not(stack<int_identifier>& operators_list, int_identifier& item1);
 
-void P_difference(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-                  stack<int_identifier> &operators_list, int c);
+void P_sum(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
 
-void P_prod(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-            stack<int_identifier> &operators_list, int c);
+void P_difference(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
 
-void P_xor(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-           stack<int_identifier> &operators_list, int c);
+void P_prod(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
 
-void P_and(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-           stack<int_identifier> &operators_list, int c);
+void P_xor(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
 
-void P_or(int num1, int num2, stack<int_identifier> &numbers_list, int_identifier &item1,
-          stack<int_identifier> &operators_list, int c);
+void P_and(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
 
-bool P_maths(stack<int_identifier> &numbers_list, stack<int_identifier> &operators_list, int_identifier &item1);
+void P_or(int num1, int num2, stack<int_identifier>& numbers_list, int_identifier& item1,
+    stack<int_identifier>& operators_list, int c);
+
+bool P_maths(stack<int_identifier>& numbers_list, stack<int_identifier>& operators_list, int_identifier& item1);
 
 bool
-P_mathsExceptions(stack<int_identifier> &numbers_list, stack<int_identifier> &operators_list, int_identifier &item1);
+P_mathsExceptions(stack<int_identifier>& numbers_list, stack<int_identifier>& operators_list, int_identifier& item1);
 void P_not(int num1, stack<int_identifier>& numbers_list, int_identifier& item1, stack<int_identifier>& operators_list, int c);
 void P_calculate();
+void P_calculatorStart();
+
