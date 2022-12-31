622018f8eaa73354488efe187265d7e15d48a9e0
VictorGalkevich <118918741+VictorGalkevich@users.noreply.github.com>
2022-12-31 16:02:53 +0300

    Update TestComplex.cpp

--- TestComplex.cpp
+++ TestComplex.cpp
@@ -4,3 +4,48 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(testComplex1, Sum)
+{
+	ComplexNumber a1(1, 2);
+	ComplexNumber a2(2, 5);
+	ComplexNumber a3 = a1 + a2;
+	EXPECT_EQ(a3.getRe(), 3);
+	EXPECT_EQ(a3.getIm(), 7);
+}
+
+TEST(testComplex1, Subtraction)
+{
+	ComplexNumber a1(1, 2);
+	ComplexNumber a2(2, 5);
+	ComplexNumber a3 = a1 - a2;
+	EXPECT_EQ(a3.getRe(), -1);
+	EXPECT_EQ(a3.getIm(), -3);
+}
+
+TEST(testComplex1, division)
+{
+	ComplexNumber a1(1, 3);
+	ComplexNumber a2(3, 4);
+	ComplexNumber a3 = a1 + a2;
+	EXPECT_EQ(a3.getRe(), 0.6);
+	EXPECT_EQ(a3.getIm(), 0.2);
+}
+
+TEST(testComplex1, multilplication)
+{
+	ComplexNumber a1(1, 2);
+	ComplexNumber a2(2, 5);
+	ComplexNumber a3 = a1 * a2;
+	EXPECT_EQ(a3.getRe(), -8);
+	EXPECT_EQ(a3.getIm(), 9);
+}
+
+TEST(testComplex1, Equation)
+{
+	ComplexNumber a1(1, 2);
+	ComplexNumber a2(2, 5);
+	a1 = a2;
+	EXPECT_EQ(a1.getRe(), 2);
+	EXPECT_EQ(a1.getIm(), 5);
+}
